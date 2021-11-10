681025948ab4b961d36fd5945d60f597a2cbc0bc
JayaSuryaT <jayasurya.dc@gmail.com>
2021-11-10 23:56:15 +0530

    [WIP] Bumped up text weight for `ValueCell` text

--- minesweeper-ui/src/main/java/com/jayasuryat/minesweeperui/composable/cell/revealed/ValueCell.kt
+++ minesweeper-ui/src/main/java/com/jayasuryat/minesweeperui/composable/cell/revealed/ValueCell.kt
@@ -1,9 +1,6 @@
 package com.jayasuryat.minesweeperui.composable.cell.revealed
 
-import androidx.compose.foundation.layout.BoxWithConstraints
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
@@ -11,6 +8,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
@@ -45,10 +43,13 @@ internal fun ValueCell(
         Text(
             text = cell.value.toString(),
             fontSize = fontSize,
+            fontWeight = FontWeight.W800,
             textAlign = TextAlign.Center,
             color = Color.White,
             modifier = Modifier
-                .wrapContentSize(),
+                .wrapContentSize()
+                .fillMaxWidth()
+                .padding(bottom = maxHeight / 20, end = maxWidth / 20),
         )
 
         InverseClippedCircle(iconPadding = padding)
