13dc7193ada87566a4e66a344268710eca583b50
JayaSuryaT <jayasurya.dc@gmail.com>
2021-11-11 18:48:09 +0530

    Update `MineCell` to bring in new hierarchy with `ValueCell`

--- minesweeper-engine/src/main/java/com/jayasuryat/minesweeperengine/model/cell/MineCell.kt
+++ minesweeper-engine/src/main/java/com/jayasuryat/minesweeperengine/model/cell/MineCell.kt
@@ -4,23 +4,30 @@ import androidx.compose.runtime.Immutable
 import com.jayasuryat.minesweeperengine.model.block.Position
 
 @Immutable
-public sealed class MineCell {
+public sealed interface MineCell {
 
-    public abstract val position: Position
+    public val position: Position
 
     @Immutable
     public data class Mine(
         override val position: Position,
-    ) : MineCell()
+    ) : MineCell
 
-    @Immutable
-    public class EmptyCell(
-        override val position: Position,
-    ) : MineCell()
+    public sealed interface ValuedCell : MineCell {
 
-    @Immutable
-    public class Cell(
-        public val value: Int,
-        override val position: Position,
-    ) : MineCell()
+        public val value: Int
+
+        @Immutable
+        public class EmptyCell(
+            override val position: Position,
+        ) : ValuedCell {
+            override val value: Int = 0
+        }
+
+        @Immutable
+        public class Cell(
+            override val value: Int,
+            override val position: Position,
+        ) : ValuedCell
+    }
 }
