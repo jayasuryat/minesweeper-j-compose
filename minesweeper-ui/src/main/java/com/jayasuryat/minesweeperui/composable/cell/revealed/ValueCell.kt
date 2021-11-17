b1773c959e39ad341e75f26ddd0552eafbcd01bf
JayaSuryaT <jayasurya.dc@gmail.com>
2021-11-17 18:30:44 +0530

    Remove click ripple for `ValueCell`

--- minesweeper-ui/src/main/java/com/jayasuryat/minesweeperui/composable/cell/revealed/ValueCell.kt
+++ minesweeper-ui/src/main/java/com/jayasuryat/minesweeperui/composable/cell/revealed/ValueCell.kt
@@ -2,10 +2,12 @@ package com.jayasuryat.minesweeperui.composable.cell.revealed
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clipToBounds
@@ -41,7 +43,10 @@ internal fun ValueCell(
         modifier = modifier
             .aspectRatio(1f)
             .clipToBounds()
-            .clickable {
+            .clickable(
+                indication = null,
+                interactionSource = remember { MutableInteractionSource() }
+            ) {
                 val action = MinefieldAction.OnValueCellClicked(cell = cell)
                 actionListener.action(action)
             },
