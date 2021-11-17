cc1804b1eba2d5f6d2bddbaabac83ab39a77ff1a
JayaSuryaT <jayasurya.dc@gmail.com>
2021-11-17 20:13:47 +0530

    Add versions plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,14 +4,19 @@ buildscript {
     repositories {
         google()
         mavenCentral()
+        gradlePluginPortal()
     }
     dependencies {
         classpath("com.android.tools.build:gradle:7.0.3")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
-
+        classpath("com.github.ben-manes:gradle-versions-plugin:0.39.0")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
+}
+
+apply {
+    from("buildScripts/versionsPlugin.gradle")
 }
 
 tasks.register("clean", Delete::class) {
