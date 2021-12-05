7ef483c90b0f8c7dd5ca65d35074139f5d8ae9c4
JayaSuryaT <jayasurya.dc@gmail.com>
2021-12-05 17:09:11 +0530

    Update dark theme colors (#7)

--- app/src/main/java/com/jayasuryat/minesweeperjc/ui/theme/Color.kt
+++ app/src/main/java/com/jayasuryat/minesweeperjc/ui/theme/Color.kt
@@ -6,3 +6,10 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val Vampire = Color(0xFF06090E)
+val Cobalt = Color(0xFF1A559E)
+val Havelock = Color(0xFF568EDC)
+val Beau = Color(0xFFB8D4F4)
+
+val Ruby = Color(0xFFAE2012)
--- app/src/main/java/com/jayasuryat/minesweeperjc/ui/theme/Theme.kt
+++ app/src/main/java/com/jayasuryat/minesweeperjc/ui/theme/Theme.kt
@@ -5,11 +5,20 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    background = Vampire,
+    onBackground = Beau,
+
+    primary = Cobalt,
+    onPrimary = Beau,
+
+    secondary = Havelock,
+    onSecondary = Beau,
+
+    error = Ruby,
+    onError = Beau,
 )
 
 private val LightColorPalette = lightColors(
@@ -30,7 +39,7 @@ private val LightColorPalette = lightColors(
 @Composable
 fun MinesweeperJCTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable () -> Unit
+    content: @Composable () -> Unit,
 ) {
     val colors = if (darkTheme) {
         DarkColorPalette
