8f6f79de39ffb1c964cfc2df00d8ea0470032835
JayaSuryaT <jayasurya.dc@gmail.com>
2021-12-05 18:50:01 +0530

    Update light theme colors (#7)

--- app/src/main/java/com/jayasuryat/minesweeperjc/ui/theme/Color.kt
+++ app/src/main/java/com/jayasuryat/minesweeperjc/ui/theme/Color.kt
@@ -2,14 +2,14 @@ package com.jayasuryat.minesweeperjc.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
-
 val Vampire = Color(0xFF06090E)
 val Cobalt = Color(0xFF1A559E)
 val Havelock = Color(0xFF568EDC)
 val Beau = Color(0xFFB8D4F4)
+
+val Beech = Color(0xFF793D01)
+val Tuscany = Color(0xFFB94E27)
+val Damask = Color(0xFFD86D46)
+val Navajo = Color(0xFFFFE2AD)
 
 val Ruby = Color(0xFFAE2012)
