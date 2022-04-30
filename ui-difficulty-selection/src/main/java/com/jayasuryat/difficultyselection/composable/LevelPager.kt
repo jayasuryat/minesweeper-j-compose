baec0d19ff0e1901f237740f8993a40d34a6f551
Jaya Surya Thotapalli <jayasurya.dc@gmail.com>
2022-04-30 21:41:53 +0530

    Move composables of `difficulty selection` screen into `composable` package

--- ui-difficulty-selection/src/main/java/com/jayasuryat/difficultyselection/composable/LevelPager.kt
+++ ui-difficulty-selection/src/main/java/com/jayasuryat/difficultyselection/composable/LevelPager.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.jayasuryat.difficultyselection
+package com.jayasuryat.difficultyselection.composable
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
