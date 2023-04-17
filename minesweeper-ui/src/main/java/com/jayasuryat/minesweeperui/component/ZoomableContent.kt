743f08e2a7e682a111fe40c2e83bd85ddfd27bf8
JayaSuryaT <jayasurya.dc@gmail.com>
2023-04-17 23:00:27 +0530

    Fix `zoom` and `click` interop issue

--- minesweeper-ui/src/main/java/com/jayasuryat/minesweeperui/component/ZoomableContent.kt
+++ minesweeper-ui/src/main/java/com/jayasuryat/minesweeperui/component/ZoomableContent.kt
@@ -15,12 +15,14 @@
  */
 package com.jayasuryat.minesweeperui.component
 
-import androidx.compose.foundation.gestures.detectTransformGestures
+import androidx.compose.foundation.gestures.*
 import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.input.pointer.*
 import com.jayasuryat.util.LogCompositions
+import kotlin.math.PI
 import kotlin.math.absoluteValue
 
 @Composable
@@ -39,7 +41,9 @@ internal fun ZoomableContent(
     Box(
         modifier = modifier
             .pointerInput(Unit) {
-                detectTransformGestures { _, pan, zoom, _ ->
+                detectTransformGestures(
+                    pass = PointerEventPass.Initial,
+                ) { _, pan: Offset, zoom: Float, _, _, changes: List<PointerInputChange> ->
 
                     val scaleValue = (scale.value * zoom)
                         .coerceAtLeast(1f)
@@ -63,6 +67,14 @@ internal fun ZoomableContent(
                     translateY.value = (translateY.value + pan.y)
                         .coerceAtLeast(-modTranslateY)
                         .coerceAtMost(modTranslateY)
+
+                    // Consume touch when multiple fingers down
+                    // This prevents click and long click if your finger touches a
+                    // button while pinch gesture is being invoked
+                    val size = changes.size
+                    if (size > 1) {
+                        changes.forEach { it.consume() }
+                    }
                 }
             }
     ) {
@@ -109,4 +121,113 @@ private fun getScaledTranslation(
     val scaledWidth = originalSize * scaleFactor
     val widthDiff = (scaledWidth - originalSize).absoluteValue
     return (widthDiff / 2)
+}
+
+// Code sourced from https://stackoverflow.com/a/76021552/10323788
+
+private suspend fun PointerInputScope.detectTransformGestures(
+    panZoomLock: Boolean = false,
+    consume: Boolean = true,
+    pass: PointerEventPass = PointerEventPass.Main,
+    onGestureStart: (PointerInputChange) -> Unit = {},
+    onGestureEnd: (PointerInputChange) -> Unit = {},
+    onGesture: (
+        centroid: Offset,
+        pan: Offset,
+        zoom: Float,
+        rotation: Float,
+        mainPointer: PointerInputChange,
+        changes: List<PointerInputChange>
+    ) -> Unit,
+) {
+
+    awaitEachGesture {
+        var rotation = 0f
+        var zoom = 1f
+        var pan = Offset.Zero
+        var pastTouchSlop = false
+        val touchSlop = viewConfiguration.touchSlop
+        var lockedToPanZoom = false
+
+        // Wait for at least one pointer to press down, and set first contact position
+        val down: PointerInputChange = awaitFirstDown(
+            requireUnconsumed = false,
+            pass = pass
+        )
+        onGestureStart(down)
+
+        var pointer = down
+        // Main pointer is the one that is down initially
+        var pointerId = down.id
+
+        do {
+            val event = awaitPointerEvent(pass = pass)
+
+            // If any position change is consumed from another PointerInputChange
+            // or pointer count requirement is not fulfilled
+            val canceled = event.changes.any { it.isConsumed }
+
+            if (!canceled) {
+
+                // Get pointer that is down, if first pointer is up
+                // get another and use it if other pointers are also down
+                // event.changes.first() doesn't return same order
+                val pointerInputChange = event.changes
+                    .firstOrNull { it.id == pointerId }
+                    ?: event.changes.first()
+
+                // Next time will check same pointer with this id
+                pointerId = pointerInputChange.id
+                pointer = pointerInputChange
+
+                val zoomChange = event.calculateZoom()
+                val rotationChange = event.calculateRotation()
+                val panChange = event.calculatePan()
+
+                if (!pastTouchSlop) {
+                    zoom *= zoomChange
+                    rotation += rotationChange
+                    pan += panChange
+
+                    val centroidSize = event.calculateCentroidSize(useCurrent = false)
+                    val zoomMotion = kotlin.math.abs(1 - zoom) * centroidSize
+                    val rotationMotion =
+                        kotlin.math.abs(rotation * PI.toFloat() * centroidSize / 180f)
+                    val panMotion = pan.getDistance()
+
+                    if (zoomMotion > touchSlop ||
+                        rotationMotion > touchSlop ||
+                        panMotion > touchSlop
+                    ) {
+                        pastTouchSlop = true
+                        lockedToPanZoom = panZoomLock && rotationMotion < touchSlop
+                    }
+                }
+
+                if (pastTouchSlop) {
+                    val centroid = event.calculateCentroid(useCurrent = false)
+                    val effectiveRotation = if (lockedToPanZoom) 0f else rotationChange
+                    if (effectiveRotation != 0f ||
+                        zoomChange != 1f ||
+                        panChange != Offset.Zero
+                    ) {
+                        onGesture(
+                            centroid,
+                            panChange,
+                            zoomChange,
+                            effectiveRotation,
+                            pointer,
+                            event.changes
+                        )
+                    }
+
+                    if (consume) {
+                        event.changes.forEach { if (it.positionChanged()) it.consume() }
+                    }
+                }
+            }
+        } while (!canceled && event.changes.any { it.pressed })
+
+        onGestureEnd(pointer)
+    }
 }
